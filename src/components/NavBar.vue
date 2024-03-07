4e6b30c3559ed1beb7b346154279fa7e1c106b3d
Tangtai-midas <natcha@midassoft.dev>
2024-03-07 16:21:01 +0700

    implement ui login page

---
+++ src/components/NavBar.vue
@@ -0,0 +1,7 @@
+<template>
+  <nav class="text-white h-[70px] p-2 text-base flex items-center justify-center">
+    Login
+    <!-- <RouterLink to="/">Home</RouterLink>
+    <RouterLink to="/about">About</RouterLink> -->
+  </nav>
+</template>
--- src/components/icons/IconSupport.vue
+++ src/components/icons/IconSupport.vue
@@ -1,7 +1,14 @@
 <template>
-  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor">
+  <svg xmlns="http://www.w3.org/2000/svg" width="2em" height="2em" viewBox="0 0 24 24">
     <path
-      d="M10 3.22l-.61-.6a5.5 5.5 0 0 0-7.666.105 5.5 5.5 0 0 0-.114 7.665L10 18.78l8.39-8.4a5.5 5.5 0 0 0-.114-7.665 5.5 5.5 0 0 0-7.666-.105l-.61.61z"
-    />
+      fill="currentColor"
+      d="M19.95 21q-3.125 0-6.175-1.362t-5.55-3.863q-2.5-2.5-3.862-5.55T3 4.05q0-.45.3-.75t.75-.3H8.1q.35 0 .625.238t.325.562l.65 3.5q.05.4-.025.675T9.4 8.45L6.975 10.9q.5.925 1.187 1.787t1.513 1.663q.775.775 1.625 1.438T13.1 17l2.35-2.35q.225-.225.588-.337t.712-.063l3.45.7q.35.1.575.363T21 15.9v4.05q0 .45-.3.75t-.75.3"
+    ></path>
   </svg>
 </template>
+
+<script lang="ts">
+export default {
+  name: 'MaterialSymbolsCall'
+}
+</script>
